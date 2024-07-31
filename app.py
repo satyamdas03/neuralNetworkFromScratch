28be495102106b0b8b948e3908889dfaf783b2bf
satyamdas03 <116027621+satyamdas03@users.noreply.github.com>
2024-07-31 21:33:14 +0530

    changing the values of the inputs, weights and bias

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
-inputs = [1.2,5.1,2.1] #unique inputs
-weights = [3.1,2.1,8.7]
-bias = 3 #each weight has a bias
+inputs = [1,2,3] #unique inputs
+weights = [0.2,0.8,-0.5]
+bias = 2 
 #pretty much the output for the neural network for now
 output = inputs[0]*weights[0] + inputs[1]*weights[1] + inputs[2]*weights[2] + bias
 print(output) 
