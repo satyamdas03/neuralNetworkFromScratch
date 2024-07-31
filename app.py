d992f80be412c19b2d0f5bd6bdde9abe0106f639
satyamdas03 <116027621+satyamdas03@users.noreply.github.com>
2024-07-31 21:39:04 +0530

    adding comments to the code

--- app.py
+++ app.py
@@ -5,3 +5,7 @@ bias = 2
 output = inputs[0]*weights[0] + inputs[1]*weights[1] + inputs[2]*weights[2] + bias
 print(output) 
 
+
+# the input values can be either just values or the layer
+# the weights are given for the synopsys
+# the bias is given for the neuron
