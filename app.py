97d5cd7542cde9e22e2e36b16bde5eb2582a78d0
satyamdas03 <116027621+satyamdas03@users.noreply.github.com>
2024-07-31 21:28:30 +0530

    finding the output for the neural network in the first step

--- app.py
+++ app.py
@@ -1,4 +1,7 @@
 inputs = [1.2,5.1,2.1] #unique inputs
 weights = [3.1,2.1,8.7]
 bias = 3 #each weight has a bias
+#pretty much the output for the neural network for now
+output = inputs[0]*weights[0] + inputs[1]*weights[1] + inputs[2]*weights[2] + bias
+print(output) 
 
