4f2d3756da07c0b0365ecc4dfb31091cd8a70a7b
satyamdas03 <116027621+satyamdas03@users.noreply.github.com>
2024-08-02 17:44:47 +0530

    updates

--- app.py
+++ app.py
@@ -11,7 +11,7 @@ bias3 = 0.5
 output = [inputs[0]*weights1[0] + inputs[1]*weights1[1] + inputs[2]*weights1[2] + inputs[3]*weights1[3] + bias1,
           inputs[0]*weights2[0] + inputs[1]*weights2[1] + inputs[2]*weights2[2] + inputs[3]*weights2[3] + bias2,
           inputs[0]*weights3[0] + inputs[1]*weights3[1] + inputs[2]*weights3[2] + inputs[3]*weights3[3] + bias3
-          ]
+        ]
 print(output) 
 
 
