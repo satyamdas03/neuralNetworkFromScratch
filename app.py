fe05912054ffab577c80e11fe629f6d40b8fdd8e
satyamdas03 <116027621+satyamdas03@users.noreply.github.com>
2024-08-02 20:12:08 +0530

    f

--- app.py
+++ app.py
@@ -2,7 +2,7 @@
 #using numpy will eliminate the use of for loops
 
 import numpy as np
-inputs = [1,2,3,2.5]
+inputs = [1,2,3,2.5] #--> features from a single sample
 weights = [[0.2,0.8,-0.5,1.0],
            [0.5, -0.91, 0.26, -0.5],
            [-.26, -0.27, 0.17, 0.87]]
