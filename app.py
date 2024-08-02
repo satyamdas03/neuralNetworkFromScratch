d9b53500a3b7e3e6369ccb46440856101f2bc657
satyamdas03 <116027621+satyamdas03@users.noreply.github.com>
2024-08-02 17:53:47 +0530

    using loops to ease the calculations

--- app.py
+++ app.py
@@ -1,18 +1,22 @@
 inputs = [1,2,3,2.5] #unique inputs
 #adding extra unique weight sets
-weights1 = [0.2,0.8,-0.5,1.0]
-weights2 = [0.5, -0.91, 0.26, -0.5]
-weights3 = [-.26, -0.27, 0.17, 0.87]
 #adding extra unique biases
-bias1 = 2 
-bias2 = 3
-bias3 = 0.5
+#using list of lists
+weights = [[0.2,0.8,-0.5,1.0],
+           [0.5, -0.91, 0.26, -0.5],
+           [-.26, -0.27, 0.17, 0.87]
+           ]
+biases = [2,3,0.5]
 #pretty much the output for the neural network for now
-output = [inputs[0]*weights1[0] + inputs[1]*weights1[1] + inputs[2]*weights1[2] + inputs[3]*weights1[3] + bias1,
-          inputs[0]*weights2[0] + inputs[1]*weights2[1] + inputs[2]*weights2[2] + inputs[3]*weights2[3] + bias2,
-          inputs[0]*weights3[0] + inputs[1]*weights3[1] + inputs[2]*weights3[2] + inputs[3]*weights3[3] + bias3
-        ]
-print(output) 
+# using loop structure to simplify the calculation
+layer_outputs = []
+for neuron_weights, neuron_bias in zip(weights, biases):
+    neuron_output = 0
+    for n_input , weight in zip(inputs,neuron_weights):
+        neuron_output += n_input*weight
+    neuron_output += neuron_bias
+    layer_outputs.append(neuron_output)
+print(layer_outputs)
 
 
 # the input values can be either just values or the layer
@@ -21,7 +25,7 @@ print(output)
 # we are going to use 4 inputs and 3 neurons, that means there are going to be 3 unique weight sets, and each weight set is going to have 4 unique values, we are going to need unique biases 
 
 # the next steps will be working the DOT PRODUCT
-    #--code
+    #--code --> ditching the repetative code 
     #--weight and bias
     #--shape
     #--dot product
