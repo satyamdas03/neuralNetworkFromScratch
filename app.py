c56a71c7bb47e316ce944993ed20ed12d064f5ed
satyamdas03 <116027621+satyamdas03@users.noreply.github.com>
2024-08-03 09:18:12 +0530

    updating the docs and converting the weight array to numpy array and using transpose on it

--- app.py
+++ app.py
@@ -2,14 +2,18 @@
 #using numpy will eliminate the use of for loops
 
 import numpy as np
-inputs = [1,2,3,2.5] #--> features from a single sample
+inputs = [[1,2,3,2.5],
+          [2.0,5.0,-1.0,2.0],
+          [-1.5,2.7,3.3,-0.8]
+          ] #--> features from a single sample
 weights = [[0.2,0.8,-0.5,1.0],
            [0.5, -0.91, 0.26, -0.5],
            [-.26, -0.27, 0.17, 0.87]]
 biases = [2,3,0.5]
-output = np.dot(weights,inputs) + biases
+output = np.dot(inputs, np.array(weights).T) + biases
 print(output)
 
 
 # if we use np.dot(inputs, weights) --> error
-# adjusting weights and biases can give us completely different
+# adjusting weights and biases can give us completely different and they are associated with individual neurons
+
