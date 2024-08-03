1ff18c6c08fe06293f07b043fd425466048dc790
satyamdas03 <116027621+satyamdas03@users.noreply.github.com>
2024-08-03 10:22:18 +0530

    coded the layers into objects

--- app.py
+++ app.py
@@ -1,27 +1,6 @@
 #using numpy to make the calculations
 #using numpy will eliminate the use of for loops
 
-import numpy as np
-inputs = [[1,2,3,2.5],
-          [2.0,5.0,-1.0,2.0],
-          [-1.5,2.7,3.3,-0.8]
-          ] #--> features from a single sample
-weights = [[0.2,0.8,-0.5,1.0],
-           [0.5, -0.91, 0.26, -0.5],
-           [-.26, -0.27, 0.17, 0.87]]
-biases = [2,3,0.5]
-
-weights2 = [[0.1,-0.14,0.5],
-           [-0.5,0.12,-0.33],
-           [-0.44,0.73,-0.13]]
-biases2 = [-1,2,-0.5]
-
-layer1_outputs = np.dot(inputs, np.array(weights).T) + biases
-layer2_outputs = np.dot(layer1_outputs, np.array(weights2).T) + biases2
-print(layer2_outputs)
-
-
-
 # if we use np.dot(inputs, weights) --> error
 # adjusting weights and biases can give us completely different and they are associated with individual neurons
 
@@ -40,9 +19,17 @@ X = [[1,2,3,2.5],
 #gonna use oops
 class Layer_Dense:
     def __init__(self, n_inputs, n_neurons):
-        self.weights = 0.10*np.random.randn(n_inputs, n_neurons) # we are gonna pass the shape inside this paranthesis, we need to know kind of 2 things, whats the size of the input coming in and how many neurons are we gonna have
+        self.weights = 0.10*np.random.randn(n_inputs, n_neurons) # we are gonna pass the shape inside this parenthesis, we need to know kind of 2 things, whats the size of the input coming in and how many neurons are we gonna have
         self.biases = np.zeros((1, n_neurons)) #since the biases is a 1d array of the number of neurons
-    def forward(self):
-        pass
+    def forward(self, inputs):
+        self.output = np.dot(inputs, self.weights)+self.biases
 # in the case of neural networks there are some ways we are gonna initialise a layer, first is we are gonna have to train the model that we have saved and we want to load in that model 
+# the output of the first layer is the input of the second layer
+layer1 = Layer_Dense(4,5) #4--> input size ; 5 is the random value
+layer2 = Layer_Dense(5,2) #5--> output of one layer is input of another
+layer1.forward(X)
+print(layer1.output)
+layer2.forward(layer1.output)
+print(layer2.output)
+
 
