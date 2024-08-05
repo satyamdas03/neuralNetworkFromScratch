290b986901158f8f300f04afa7b50af19c47f489
satyamdas03 <116027621+satyamdas03@users.noreply.github.com>
2024-08-05 11:00:45 +0530

    coding up the activation function for the neural network

--- app.py
+++ app.py
@@ -10,12 +10,17 @@
 ## coding up the above code, converting the layers into objects
 
 import numpy as np
-np.random.seed(0)
+import nnfs
+from nnfs.datasets import spiral_data
+
+nnfs.init() #setting up a default datatype for the numpy use
+
 X = [[1,2,3,2.5],
     [2.0,5.0,-1.0,2.0],
     [-1.5,2.7,3.3,-0.8]
           ] #--> features from a single sample
 
+X, y = spiral_data(100,3) #100 feature sets of 3 classes
 #gonna use oops
 class Layer_Dense:
     def __init__(self, n_inputs, n_neurons):
@@ -23,6 +28,12 @@ class Layer_Dense:
         self.biases = np.zeros((1, n_neurons)) #since the biases is a 1d array of the number of neurons
     def forward(self, inputs):
         self.output = np.dot(inputs, self.weights)+self.biases
+
+#coding up the activation function
+class Activation_ReLU:
+    def forward(self,inputs):
+        self.output = np.maximum(0, inputs)
+
 # in the case of neural networks there are some ways we are gonna initialise a layer, first is we are gonna have to train the model that we have saved and we want to load in that model 
 # the output of the first layer is the input of the second layer
 layer1 = Layer_Dense(4,5) #4--> input size ; 5 is the random value
