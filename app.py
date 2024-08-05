0275e778b4ee2879af799dfb286fc257f9568f63
satyamdas03 <116027621+satyamdas03@users.noreply.github.com>
2024-08-05 11:11:58 +0530

    coded the activatiion function for the neural network

--- app.py
+++ app.py
@@ -1,45 +1,34 @@
-#using numpy to make the calculations
-#using numpy will eliminate the use of for loops
-
-# if we use np.dot(inputs, weights) --> error
-# adjusting weights and biases can give us completely different and they are associated with individual neurons
-
-#the layer1_output is the output for layer 1 which is then the input of layer2
-# hidden layers are called so because, we as programmers are not really in charge of how that layer changes
-
-## coding up the above code, converting the layers into objects
-
 import numpy as np
 import nnfs
 from nnfs.datasets import spiral_data
 
-nnfs.init() #setting up a default datatype for the numpy use
+nnfs.init()  # Setting up a default datatype for the numpy use
 
-X = [[1,2,3,2.5],
-    [2.0,5.0,-1.0,2.0],
-    [-1.5,2.7,3.3,-0.8]
-          ] #--> features from a single sample
+X, y = spiral_data(100, 3)  # 100 feature sets of 3 classes
 
-X, y = spiral_data(100,3) #100 feature sets of 3 classes
-#gonna use oops
+# Define the Layer_Dense class
 class Layer_Dense:
     def __init__(self, n_inputs, n_neurons):
-        self.weights = 0.10*np.random.randn(n_inputs, n_neurons) # we are gonna pass the shape inside this parenthesis, we need to know kind of 2 things, whats the size of the input coming in and how many neurons are we gonna have
-        self.biases = np.zeros((1, n_neurons)) #since the biases is a 1d array of the number of neurons
-    def forward(self, inputs):
-        self.output = np.dot(inputs, self.weights)+self.biases
+        self.weights = 0.10 * np.random.randn(n_inputs, n_neurons)
+        self.biases = np.zeros((1, n_neurons))
 
-#coding up the activation function
+    def forward(self, inputs):
+        self.output = np.dot(inputs, self.weights) + self.biases
+
+# Define the Activation_ReLU class
 class Activation_ReLU:
-    def forward(self,inputs):
+    def forward(self, inputs):
         self.output = np.maximum(0, inputs)
 
-# in the case of neural networks there are some ways we are gonna initialise a layer, first is we are gonna have to train the model that we have saved and we want to load in that model 
-# the output of the first layer is the input of the second layer
-layer1 = Layer_Dense(4,5) #4--> input size ; 5 is the number of neurons
+# Initialize the layers
+layer1 = Layer_Dense(2, 5)  # 2 input features (matches the dataset), 5 neurons
 activation1 = Activation_ReLU()
 
+# Perform a forward pass
 layer1.forward(X)
-print(layer1.output)
+activation1.forward(layer1.output)
 
-
+# Print the output of the first layer
+#print(layer1.output)
+activation1.forward(layer1.output)
+print(activation1.output)
