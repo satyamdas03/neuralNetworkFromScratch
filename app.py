4640ad282fb9851343916382c7d4f156df2cebaa
satyamdas03 <116027621+satyamdas03@users.noreply.github.com>
2024-08-07 20:53:48 +0530

    coding up the softmax function for layered outputs

--- app.py
+++ app.py
@@ -36,18 +36,14 @@ print(activation1.output)
 
 #coding up the softmax function(following the steps inside the documents.txt)
 
-## this part is the exponatiating part
-import math
-layer_outputs = [4.8,1.21,2.385]
-E = math.e
-exp_values = []
-for output in layer_outputs:
-    exp_values.append(E**output)
-print(exp_values)
-
-## next we will be normalizing the exponitiated values
-norm_base = sum(exp_values)
-norm_values = []
-for value in exp_values:
-    norm_values.append(value/norm_base)
+import numpy as np
+import nnfs
+nnfs.init()
+layer_outputs = [[4.8,1.21,2.385],
+                 [8.9, -1.81, 0.2],
+                 [1.41, 1.051, 0.026]]
+exp_values = np.exp(layer_outputs)
+norm_values = exp_values/np.sum(exp_values, axis=1,keepdims=True) 
+##np.sum(exp_values, axis=1, keepdims=True) 
+##axis=1 --> for row wise sums, keepdims=True --> for printing the sums output in the layer_outputs shape
 print(norm_values)
