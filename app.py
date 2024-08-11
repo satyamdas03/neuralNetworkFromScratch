819def620b40cc0158dc336baae75863016790b9
satyamdas03 <116027621+satyamdas03@users.noreply.github.com>
2024-08-11 18:24:57 +0530

    coding up the loss function

--- app.py
+++ app.py
@@ -24,6 +24,26 @@ class Activation_Softmax:
         probabilities = exp_values/np.sum(exp_values,axis=1,keepdims=True)
         self.output = probabilities
 
+class Loss:
+    def calculate(self, output, y):
+        sample_losses = self.forward(output,y)
+        data_loss = np.mean(sample_losses)
+        return data_loss
+
+class Loss_CategoricalCrossEntropy(Loss): #inherited from the Loss Class
+    def forward(self, y_pred, y_true):
+        samples = len(y_pred)
+        y_pred_clipped = np.clip(y_pred,1e-7,1-1e-7)
+
+        if len(y_true.shape)==1:
+            correct_confidences = y_pred_clipped[range(samples), y_true]
+        elif len(y_true.shape)==2:
+            correct_confidences = np.sum(y_pred_clipped*y_true, axis=1)
+        negative_log_likelihoods = -np.log(correct_confidences)
+        return negative_log_likelihoods
+
+
+
 X,y = spiral_data(samples=100,classes=3)
 dense1 = Layer_Dense(2,3)
 activation1 = Activation_ReLU()
@@ -39,3 +59,8 @@ activation2.forward(dense2.output)
 
 print(activation2.output[:5])
 ## GONNA HAVE TO UPDATE THE README FILE
+
+loss_function = Loss_CategoricalCrossEntropy()
+loss = loss_function.calculate(activation2.output, y)
+
+print("loss:", loss)
